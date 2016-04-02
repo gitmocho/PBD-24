e1e24815bdf906f8218ce4006c38ad56e5217c86
mocho <mocho@live.co.uk>
2016-04-02 06:21:32 +0100

    First commit]

---
+++ PBD24.py
@@ -0,0 +1,30 @@
+
+#description     :PBD-24 Create a dictionary containing min 15 items/results.
+#author          :MC
+#date            :02/04/2014
+#version         :0.1
+#usage           :python pyscript.py
+#notes           :
+#python_version  :3.4.2 
+ 
+ 
+countriesOfTheWorld = {
+'Afghanistan':'AFG',
+'Aland Islands':'ALA',
+'Albania':'ALB',
+'Algeria':'DZA',
+'American Samoa':'ASM',
+'Andorra':'AND',
+'Angola':'AGO',
+'Anguilla':'AIA',
+'Antarctica':'ATA',
+'Antigua and Barbuda':'ATG',
+'Argentina':'ARG',
+'Armenia':'ARM',
+'Aruba':'ABW',
+'Australia':'AUS',
+'Austria':'AUT',
+}
+ 
+ 
+# print(countriesOfTheWorld['American Samoa'])
